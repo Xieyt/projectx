d3f37c9df98f0249b6cd9d16cff7bfb434929ba0
Xieyt <alokmilenium@gmail.com>
2023-02-01 17:03:16 +0530

    Done: - Mailhog configuration Done - installed and Configured mhsendmail

--- src/index.php
+++ src/index.php
@@ -1,3 +1,10 @@
  <?php
-phpinfo();
+$from = "emailtest@YOURDOMAIN";
+$to = "YOUREMAILADDRESS";
+$subject = "PHP Mail Test script";
+$message = "This is a test to check the PHP Mail functionality";
+$headers = "From:" . $from;
+mail($to,$subject,$message, $headers);
+echo "Test email sent";
+# phpinfo();
 ?>
