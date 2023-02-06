c0c70e35dd4c905f7d4d9abb855da40eae14e29b
Xieyt <alokmilenium@gmail.com>
2023-02-06 14:23:43 +0530

    All done

--- src/index.php
+++ src/index.php
@@ -1,10 +1,2 @@
- <?php
-$from = "emailtest@YOURDOMAIN";
-$to = "YOUREMAILADDRESS";
-$subject = "PHP Mail Test script";
-$message = "This is a test to check the PHP Mail functionality";
-$headers = "From:" . $from;
-mail($to,$subject,$message, $headers);
-echo "Test email sent";
-# phpinfo();
-?>
+<?php
+phpinfo();
